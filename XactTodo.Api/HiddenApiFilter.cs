86595770ecb0ca3b69711857765cc8154f7ab05f
xuzicheng <xzc@cohl.com>
2024-07-06 08:17:31 +0800

    添加Swagger支持

---
+++ XactTodo.Api/HiddenApiFilter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Linq;
+
+namespace XactTodo.Api
+{
+    /// <summary> 
+    /// 隐藏接口，不生成到swagger文档展示 
+    /// 注意：如果不加[HiddenApi]标记的接口名称和加过标记的隐藏接口名称相同，则该普通接口也会被隐藏不显示，所以建议接口名称最好不要重复
+    /// </summary> 
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public partial class HiddenApiAttribute : Attribute { }
+
+    public class HiddenApiFilter : IDocumentFilter
+    {
+
+        /// <summary> 
+        /// 重写Apply方法，移除隐藏接口的生成 
+        /// </summary> 
+        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+        {
+            foreach (var apiDescription in context.ApiDescriptions)
+            {
+                if (apiDescription.CustomAttributes().Any(a=>a.GetType()==typeof(HiddenApiAttribute)))
+                {
+                    string key = "/" + apiDescription.RelativePath;
+                    if (key.Contains("?"))
+                    {
+                        int idx = key.IndexOf("?", StringComparison.Ordinal);
+                        key = key.Substring(0, idx);
+                    }
+                    swaggerDoc.Paths.Remove(key);
+                }
+            }
+        }
+    }
+}
--- XactTodo.Api/Program.cs
+++ XactTodo.Api/Program.cs
@@ -1,7 +1,11 @@
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.OpenApi.Models;
 using NLog;
 using NLog.Web;
+using Swashbuckle.AspNetCore.Filters;
 using XactTodo.Api.Authentication;
 using XactTodo.Api.Controllers;
 using XactTodo.Api.Queries;
@@ -13,6 +17,8 @@ namespace XactTodo.Api
 {
     public class Program
     {
+        private static readonly string swaggerDocName = "v1";
+        
         public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -31,7 +37,8 @@ namespace XactTodo.Api
             {
                 var context = scope.ServiceProvider.GetRequiredService<TodoContext>();
                 await context.Database.MigrateAsync(); // 自动迁移数据库
-            }catch(Exception ex)
+            }
+            catch (Exception ex)
             {
                 logger.LogError("执行数据库迁移失败，异常信息：" + ex.Message + Environment.NewLine + "ConnectionString:" + connectionString);
                 return;
@@ -51,15 +58,28 @@ namespace XactTodo.Api
             builder.Services.AddControllers().AddNewtonsoftJson();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            //Swagger支持
+            bool.TryParse(builder.Configuration.GetSection("SwaggerDoc:Enabled").Value ?? "true", out bool swaggerEnabled);
+            if (swaggerEnabled)
+            {
+                AddSwagger(builder.Configuration, builder.Services);// app.Environment);
+            }
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (swaggerEnabled)//if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint(
+                        // url: 需配合 SwaggerDoc 的 name。 "/swagger/{SwaggerDoc name}/swagger.json"
+                        url: $"../swagger/{swaggerDocName}/swagger.json", //这里一定要使用相对路径，不然网站发布到子目录时将报告："Not Found /swagger/v1/swagger.json"
+                        // description: 用於 Swagger UI 右上角選擇不同版本的 SwaggerDocument 顯示名稱使用。
+                        name: "RESTful API v1.0.0"
+                    );
+                });
             }
 
             app.UseCors(policy =>
@@ -75,5 +95,57 @@ namespace XactTodo.Api
 
             app.Run();
         }
+
+        public static void AddSwagger(ConfigurationManager configuration, IServiceCollection services)
+        {
+            services.AddSwaggerGenNewtonsoftSupport(); //支持通过JsonProperty定义别名
+            services.AddSwaggerGen(options =>
+            {
+                var info = new OpenApiInfo
+                {
+                    Version = configuration.GetSection("SwaggerDoc:Version").Value,
+                    Title = configuration.GetSection("SwaggerDoc:Title").Value,
+                    Description = configuration.GetSection("SwaggerDoc:Description").Value,
+                };
+                //读取appsettings.json文件中的联系人信息
+                string contactName = configuration.GetSection("SwaggerDoc:ContactName").Value;
+                string contactNameEmail = configuration.GetSection("SwaggerDoc:ContactEmail").Value;
+                string s = configuration.GetSection("SwaggerDoc:ContactUrl").Value;
+                Uri contactUrl = string.IsNullOrEmpty(s) ? null : new Uri(s);
+                if (!string.IsNullOrEmpty(contactName))
+                    info.Contact = new OpenApiContact { Name = contactName, Email = contactNameEmail, Url = contactUrl };
+                //info.License = new OpenApiLicense { Name = contactName, Url = contactUrl }
+                options.SwaggerDoc(swaggerDocName, info);
+
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, "Api.xml");
+                if (File.Exists(xmlPath))
+                    options.IncludeXmlComments(xmlPath);
+
+                options.DocumentFilter<HiddenApiFilter>(); // 在接口类、方法标记属性 [HiddenApi]，可以阻止【Swagger文档】生成
+                options.OperationFilter<AddResponseHeadersFilter>();
+                options.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();
+                //options.OperationFilter<SecurityRequirementsOperationFilter>();
+                var security = new OpenApiSecurityRequirement
+                {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+                            },
+                            new[] { "readAccess", "writeAccess" }
+                        }
+                };
+                options.AddSecurityRequirement(security);//添加一个必须的全局安全信息，和AddSecurityDefinition方法指定的方案名称要一致，这里是Bearer。
+                //给api添加token令牌证书
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT授权(数据将在请求头中进行传输) 直接在下框中输入Bearer {token}（注意两者之间是一个空格）\"",
+                    Name = "Authorization",//jwt默认的参数名称
+                    In = ParameterLocation.Header,//jwt默认存放Authorization信息的位置(请求头中)
+                    Type = SecuritySchemeType.ApiKey
+                });
+            });
+        }
+
     }
 }
